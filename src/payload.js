7cd563dff528fc6814c6be5f6f8b45cd767cf6e5
LandRover <github.com@landroverz.com>
2016-01-17 16:23:25 +0000

    mocha set

--- src/payload.js
+++ src/payload.js
@@ -41,6 +41,7 @@ class Payload {
         return message;
     }
     
+    
     /**
      *
      */
---
+++ test/payload.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let chai = require('chai');
+let expect = require('chai').expect;
+
+describe('Payload', () => {
+    it('returns a string', () => {
+        var t = 'h1';
+        
+        expect(t).to.equal('h1');
+        
+    });
+});
