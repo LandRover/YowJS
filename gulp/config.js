6456f461329ce82fa743a144a39fa75ddd634d3a
LandRover <github.com@landroverz.com>
2016-01-25 16:32:33 +0000

    eslint

---
+++ gulp/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    path: {
+        js: {
+            files: 'src/js/**/*.js'
+        },
+        tasks: {
+            files: 'gulp/**/*.js'
+        },
+        test: {
+            files: 'test/**/*.spec.js'
+        },
+        modules: {
+            npm: 'node_modules/**'
+        }
+    },
+
+    jscpd: {
+        'min-lines': 5,
+        verbose: true
+    },
+
+    messages: {
+        error: 'Error: <%= error.message %> on file <%= file.relative %>',
+        success: 'Success: File <%= file.relative %> gulped'
+    }
+};
---
+++ gulp/tasks/eslint.js
@@ -0,0 +1,31 @@
+'use strict';
+/**
+ *
+ */
+const gulp = require('gulp'),
+      eslint = require('gulp-eslint'),
+      config = require('./../config');
+
+gulp.task('eslint:js', () => {
+    return gulp.src([config.path.js.files])
+        .pipe(eslint({
+            envs: [
+                'node'
+            ],
+            useEslintrc: true
+        }))
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
+});
+
+gulp.task('eslint:tasks', () => {
+    return gulp.src(config.path.tasks.files)
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
+});
+
+gulp.task('eslint:tests', () => {
+    return gulp.src(config.path.tests.files)
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
+});
